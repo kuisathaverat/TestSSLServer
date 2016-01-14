66524e193b78a1c229c6e5d7b1360e6a780b850d
Ivan Fernandez Calvo <inifc@Ivans-MacBook-Pro.local>
2016-01-15 00:15:14 +0100

    new references on README, worflow groovy to execute from jenkins workflow

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ https://github.com/UniconLabs/java-keystore-ssl-test
 http://docs.oracle.com/javase/7/docs/technotes/guides/security/jsse/ReadDebug.html
 https://docs.oracle.com/javase/7/docs/technotes/guides/security/jsse/samples/sockets/client/SSLSocketClientWithClientAuth.java
 
-
+also see
+http://www.smartjava.org/content/how-analyze-java-ssl-errors
---
+++ flow.groovy
@@ -0,0 +1,26 @@
+/**
+ @author kuisatahverat
+ **/
+env.targetHost = "google.com"
+stage 'Env'
+node {
+   sh 'export'
+}
+stage 'Download sources'
+node {
+   tool name: 'Default', type: 'hudson.plugins.git.GitTool'
+    git 'https://github.com/kuisathaverat/TestSSLServer.git'
+}
+stage 'Compile sources'
+node {
+   tool name: 'maven3', type: 'hudson.tasks.Maven$MavenInstallation'
+   sh 'mvn clean compile'
+}
+stage 'Execute Test SSL support features'
+node {
+   sh 'mvn -Djavax.net.debug=ssl:handshake -Dexec.mainClass=org.bolet.TestSSLServer -Dexec.args="$targetHost" exec:java'
+}
+stage 'Execute Test SSL handshake'
+node {
+   sh 'mvn -Djavax.net.debug=ssl:handshake -Dexec.mainClass=org.bolet.TestSSLHandShake -Dexec.args="https://$targetHost" exec:java'
+}
